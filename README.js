6f3e036460dfc08c026aad6ca1f02569a510b4e6
SteveMc <contact@stevemcarthur.co.uk>
2021-02-04 14:13:25 +0000

    rename README

--- README.js
+++
@@ -1,3 +0,0 @@
-<h1 id="etsy-api">etsy-api</h1>
-<p><a href="https://travis-ci.com/SteveMcArthur/etsy-api"><img src="https://travis-ci.com/SteveMcArthur/etsy-api.svg?branch=master" alt="Build Status"></a></p>
-<p>NodeJS wrapper for the etsy v2 api.</p>
---
+++ README.md
@@ -0,0 +1,4 @@
+# etsy-api
+[![Build Status](https://travis-ci.com/SteveMcArthur/etsy-api.svg?branch=master)](https://travis-ci.com/SteveMcArthur/etsy-api)
+
+NodeJS wrapper for the etsy v2 api.
